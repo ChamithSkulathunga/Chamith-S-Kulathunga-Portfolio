c9ae0f0fe88d35f1690c97393c796f55caabbc82
Chamith S kulathunga <skchamith14@gmail.com>
2025-05-21 23:25:52 +0530

    add new features

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,12 +11,14 @@ const ovo = Ovo({
 
 export const metadata = {
   title: "Portfolio - Chamith S Kulathunga",
-  description: "Explore the portfolio of Chamith Sadeepa Kulathunga, an ICT undergraduate skilled in UI/UX design and front-end development."
+  description: "Explore the portfolio of Chamith Sadeepa Kulathunga, an ICT undergraduate skilled in UI/UX design and front-end development.",
+  icon: "/codesoft.png",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth ">
+     
       <body className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}>
         {children}
       </body>
